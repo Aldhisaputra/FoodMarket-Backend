80a82b29b8f26baa5318607553f88238ef546ac6
aldhi saputra <aldhisaputra795@gmail.com>
2024-01-10 21:01:15 +0700

    new

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,13 @@ Route::get('/', function () {
 Route::get('/debug-sentry', function () {
     throw new Exception('My first Sentry error');
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
