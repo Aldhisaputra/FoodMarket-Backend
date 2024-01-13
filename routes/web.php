0cb7038e218ea9d543100941cdcb6becc55d70f9
aldhi saputra <aldhisaputra795@gmail.com>
2024-01-13 20:04:07 +0700

    Halaman success,error,unfinish

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\MidtransController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,3 +31,8 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+
+Route::get('midtrans/success', [MidtransController::class,'success']);
+Route::get('midtrans/unfinish', [MidtransController::class,'unfinish']);
+Route::get('midtrans/error', [MidtransController::class,'error']);
