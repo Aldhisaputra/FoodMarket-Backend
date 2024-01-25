754932dd25afab04a91138d9ebba410eec998579
aldhi saputra <aldhisaputra795@gmail.com>
2024-01-25 10:22:39 +0700

    memperbaiki welcome.blade style tailwindcss& mengubah admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('admin-dashboard');
+    return redirect()->route('dashboard');
 });
 
 Route::prefix('dashboard')
 ->middleware(['auth:sanctum','admin'])
 ->group(function() {
-    Route::get('/',[DashboardController::class, 'index'])->name('admin-dashboard');
+    Route::get('/',[DashboardController::class, 'index'])->name('dashboard');
 });
 
 
