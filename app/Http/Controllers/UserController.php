27ed5043c4e0d94714b480f18d06651f396a9ab7
aldhi saputra <aldhisaputra795@gmail.com>
2024-02-01 09:24:37 +0700

    menambahkan UserController ke route & Menambahkan kode UserController Di class index

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -11,7 +12,11 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $user = User::paginate(10);
+
+        return view('user.index',[
+            'user' => $user
+        ]);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\MidtransController;
+use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,6 +24,7 @@ Route::prefix('dashboard')
 ->middleware(['auth:sanctum','admin'])
 ->group(function() {
     Route::get('/',[DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('user', UserController::class);
 });
 
 
