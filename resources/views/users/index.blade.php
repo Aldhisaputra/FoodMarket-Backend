eaa2ea769841dba52f3f72bf4c962737430a7446
aldhi saputra <aldhisaputra795@gmail.com>
2024-02-02 10:05:10 +0700

    menambhkan kodingan di index.balde .php dan warna bg belum bisa tampil

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,9 +7,63 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            {{-- Create User Button --}}
             <div class="mb-10">
                 <a href="{{ route('users.create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
                     + Create User
                 </a>
             </div>
+            
+            {{-- User Table --}}
+            <div class="bg-white">
+                <table class="table-auto w-full">
+                    <thead>
+                        <tr>
+                            <th class="border px-6 py-4">ID</th>
+                            <th class="border px-6 py-4">Name</th>
+                            <th class="border px-6 py-4">Email</th>
+                            <th class="border px-6 py-4">Roles</th>
+                            <th class="border px-6 py-4">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {{-- User Data Loop --}}
+                        @forelse($user as $item)
+                            <tr>
+                                <td class="border px-6 py-4">{{ $item->id }}</td>
+                                <td class="border px-6 py-4">{{ $item->name }}</td>
+                                <td class="border px-6 py-4">{{ $item->email }}</td>
+                                <td class="border px-6 py-4">{{ $item->roles }}</td>
+                                <td class="border px-6 py- text-center">
+                                    {{-- Edit User Button --}}
+                                    <a href="{{ route('users.edit', $item->id) }}" class="inline-block bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mx-2 rounded">
+                                        Edit
+                                    </a>
+                                    
+                                    {{-- Delete User Form --}}
+                                    <form action="{{ route('users.destroy', $item->id) }}" method="POST" class="inline-block">
+                                        {!! method_field('delete') . csrf_field() !!}
+                                        <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mx-2 rounded inline-block">
+                                            Delete
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="5" class="border text-center p-5">
+                                    Data Tidak Ditemukan
+                                </td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+            
+            {{-- Pagination Links --}}
+            <div class="text-center mt-5">
+                {{ $user->links() }}
+            </div>
+        </div>
+    </div>
 </x-app-layout>
