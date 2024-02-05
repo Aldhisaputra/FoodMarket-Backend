a0a13b951f82770dac6228802cbb0802d1362ef6
aldhi saputra <aldhisaputra795@gmail.com>
2024-02-05 10:15:51 +0700

    Menambahkan warna green di tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
 
-/** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
@@ -13,11 +10,25 @@ export default {
 
     theme: {
         extend: {
+            colors: {
+                green: {
+                    '50': '#f0fdf4',
+                    '100': '#dcfce7',
+                    '200': '#bbf7d0',
+                    '300': '#86efac',
+                    '400': '#4ade80',
+                    '500': '#22c55e',
+                    '600': '#16a34a',
+                    '700': '#15803d',
+                    '800': '#166534',
+                    '900': '#14532d',
+                },
+            },
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
     },
 
-    plugins: [forms, typography],
+    plugins: [],
 };
