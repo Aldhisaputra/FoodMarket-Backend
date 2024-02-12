f13a6b9d5fb7023488b7546b0547776b9355bc36
aldhi saputra <aldhisaputra795@gmail.com>
2024-02-12 20:28:34 +0700

    crud delet

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -77,8 +77,10 @@ class UserController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return redirect()->route('users.index');
     }
 }
